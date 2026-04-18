6c04cb105392b10b71f26c75201e73cb4e1bee18
reiyszw <reiyszw@gmail.com>
2026-04-18 20:05:15 +0900

    Working with Effect Dependencies & useCallback

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,10 +4,12 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
+    console.log("SETTING TIMEOUT");
     setTimeout(onTimeout, timeout);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
+    console.log("SETTING INTERVAL");
     setInterval(() => {
       setRemainingTime((preRemainingTime) => preRemainingTime - 100);
     }, 100);
